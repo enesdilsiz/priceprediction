e9621874752998a4ed4cf8dc742efeacf80fcbe4
enesdilsiz <54660460+enesdilsiz@users.noreply.github.com>
2022-05-11 00:03:24 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# priceprediction
-Product Price Prediction
+## ML Model Visualization Flask Deployment
+This is a demo project to elaborate how Machine Learn Models are deployed on production using Flask API
+
+### Prerequisites
+You must have Scikit Learn, Plotly, Pandas (for Machine Leraning Model) and Flask (for API) installed.
+
+### Project Structure
+This project has two major parts :
+app.py - This contains Flask APIs that receives employee details through GUI or API calls, computes the precited value based on our model and returns it.
+templates - This folder contains the HTML template to allow user to enter employee detail and displays the predicted employee salary.
+
+### Running the project
+
+python app.py
+
+By default, flask will run on port 5000.
+
+Navigate to URL http://localhost:5000
---
+++ app.py
@@ -0,0 +1,107 @@
+import numpy as np
+import plotly.utils
+from flask import Flask, request, jsonify, render_template
+#import pickle
+import pandas as pd
+import plotly
+import plotly.graph_objects as go
+import plotly.io as pio
+
+
+pio.renderers.default = 'browser'
+
+"""     ['plotly_mimetype', 'jupyterlab', 'nteract', 'vscode',
+         'notebook', 'notebook_connected', 'kaggle', 'azure', 'colab',
+         'cocalc', 'databricks', 'json', 'png', 'jpeg', 'jpg', 'svg',
+         'pdf', 'browser', 'firefox', 'chrome', 'chromium', 'iframe',
+         'iframe_connected', 'sphinx_gallery', 'sphinx_gallery_png']
+"""
+
+app = Flask(__name__)
+#model = pickle.load(open('model.pkl', 'rb'))
+
+urunler = ['kıyma', 'tavuk', 'hindi', 'Bakla-kuru', 'Bakla-taze', 'Bamya',
+               'Barbunya-kuru', 'Bezelye-taze', 'Biber-yeşil', 'Brüksel laha.',
+               'Domates', 'Enginar', 'Fasulye-kuru', 'Fasulye-taze', 'Havuç',
+               'Salatalık', 'Ispanak', 'Kabak', 'Karnabahar', 'Kereviz-baş',
+               'Kırmızı-pancar', 'Lahana', 'Mantar', 'Marul', 'Mercimek-kuru',
+               'Nohut', 'Pancar', 'Patates', 'Patlıcan', 'Pazı', 'Pırasa',
+               'Semizotu', 'Buğday ekmeği', 'Bulgur', 'Erişte', 'Makarna',
+               'Mısır', 'Mısır unu', 'Nişasta', 'Pilav', 'Pirinç unu', 'Şehriye',
+               'Tarhana', 'Yulaf unu', 'yumurta', 'beyaz peynir', 'Kaşar peyniri',
+               'krema', 'yoğurt', 'sucuk', 'salam', 'sosis', 'tam buğday ekmek',
+               'kaymak', 'bal', 'tereyağ', 'Armut', 'Çilek', 'Elma',
+               'Erik türleri', 'İncir', 'Karadut', 'Karpuz', 'Kavun', 'Kayısı',
+               'Kiraz', 'Limon', 'Mandalina', 'Muz', 'Nar', 'Portakal', 'Şeftali',
+               'Üzüm', 'Vişne', 'fındık', 'fıstık', 'kaju', 'badem', 'ceviz']
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', urunler=urunler)
+
+
+@app.route('/',methods=['POST'])
+def predict():
+
+    product_name = request.form['urun']
+    #products_name = [x for x in request.form.values()]
+    print(product_name)
+    print('######################################')
+
+    results = pd.read_csv(r'C:\Users\enesd\Desktop\priceprediction\webapp\data\predictions.csv', index_col='tarih')
+    real = pd.read_csv(r'C:\Users\enesd\Desktop\priceprediction\webapp\data\realdata.csv', index_col='tarih')
+    results2 = results[results['ürün'] == product_name]
+    temp = real[real["ürün"] == product_name]
+    whole = temp.groupby(temp.index).mean()
+
+
+    # set up plotly figure
+    fig = go.Figure()
+
+    # add line / trace 1 to figure
+    fig.add_trace(go.Scatter(
+        x=results2.index,
+        y=results2['ürün fiyatı'],
+        hovertext=results2['ürün fiyatı'],
+        hoverinfo="text",
+        name='Tahmin',
+        marker=dict(
+            color="red"
+        ),
+        showlegend=True
+    ))
+
+    # add line / trace 2 to figure
+    fig.add_trace(go.Scatter(
+        x=whole.index,
+        y=whole['ürün fiyatı'],
+        hovertext=whole['ürün fiyatı'],
+        hoverinfo="text",
+        name='Gerçek Değerler',
+        marker=dict(
+            color="green"
+        ),
+        showlegend=True
+    ))
+
+    fig.update_layout(
+        title=f"{product_name} için Fiyat Tahmini",
+        xaxis_title="Tarih",
+        yaxis_title="Ürün Fiyatı (TL)",
+        template="ggplot2",
+        font=dict(
+            family="Courier New, monospace",
+            size=18,
+            color="RebeccaPurple"
+        ))
+
+    #fig.show()
+    fig.write_html('figure.html', auto_open=True)
+
+    return render_template('index.html', urunler=urunler)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
